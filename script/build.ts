65fd444ae768a6493b827920cf71c67f3023c801
Jamyth Luk <jamythluk@jamyths-mbp.mynet>
2021-03-13 16:34:05 +0800

    [SYSTEM]: 1.2.4: build package

--- script/build.ts
+++ script/build.ts
@@ -58,10 +58,10 @@ function publish() {
     const location = path.join(__dirname, "../package.json");
     const rawPackageJSON = fs.readFileSync(location, {encoding: "utf-8"});
     const parsedJSON = JSON.parse(rawPackageJSON);
-    // const version = parseInt(parsedJSON.version[parsedJSON.version.length - 1]) + 1;
+    const version = parseInt(parsedJSON.version[parsedJSON.version.length - 1]) + 1;
+    parsedJSON.version = parsedJSON.version.substring(0, parsedJSON.version.length - 1) + version;
     console.log(parsedJSON.version);
-    // parsedJSON.version = parsedJSON.version.subString(0, parsedJSON.version.length - 1) + version;
-    // fs.writeFileSync(location, JSON.stringify(parsedJSON, null, 4), {encoding: "utf-8"});
+    fs.writeFileSync(location, JSON.stringify(parsedJSON, null, 4), {encoding: "utf-8"});
 
     // return spawn("git", ["push", "-u", "origin", "master"], "cannot push to github", true);
 }
